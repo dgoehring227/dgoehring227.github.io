155bcd004277d8018ec9da04b0e127a05b4b3971
dgoehring227 <81451600+dgoehring227@users.noreply.github.com>
2023-03-24 16:19:51 -0400

    Add files via upload

---
+++ Sctipt.js
@@ -0,0 +1,80 @@
+function showDropdown() {
+    document.getElementById("myDropdown").classList.toggle("show")
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.fa-bars')) {
+        var dropdown = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdown.length; i++) {
+            var openDropdown = dropdown[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show')
+            }
+        }
+    }
+}
+
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+      $('.hideme').each(function(i) {
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        if (bottom_of_window > bottom_of_object) {
+          $(this).animate({
+            'opacity': '1'
+          }, 500);
+        }
+      });
+    });
+  });  
+
+
+
+  function dropdownFunction() {
+    var dropdownMenu = document.getElementById("dropdown-categories");
+    if (dropdownMenu.style.display === "block") {
+      dropdownMenu.style.display = "none";
+    } else {
+      dropdownMenu.style.display = "block";
+    }
+  }
+
+  $(document).mouseup(function(e) {
+    var container = $("#dropdown-categories");
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+        container.hide();
+    }
+});
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("productimg");
+  let dots = document.getElementsByClassName("demo");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "inline";
+  dots[slideIndex-1].className += " active";
+}
+
+    
+  
